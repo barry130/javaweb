442e113f66767c709f92c27a79f2ac0f1296d511
canace <mail@canace.cn>
2020-06-29 20:19:19 +0800

    限制每天只能打卡一次

--- src/dao/BaseDao.java
+++ src/dao/BaseDao.java
@@ -39,7 +39,7 @@ public class BaseDao {
     public static boolean login(String name, String pass) {
         String sql = "select username,password from user where username='" + name + "' and password='" + pass + "' LIMIT 1";
 
-        System.err.println(sql);
+   //     System.err.println(sql);
         boolean result = executeQuery(sql);
         if (result) {
             System.err.println("登录成功,username=" + name + ",password=" + pass);
@@ -59,13 +59,28 @@ public class BaseDao {
 
     }
 
+    public static boolean isdaka(String name,String time1,String time2)
+    {
+        String sql="select * from daka where username='" + name + "'AND dakatime between '"+ time1 +"' AND '"+ time2 +"' ";
+        System.err.println(sql);
+
+        boolean result = executeQuery(sql);
+        if (result) {
+            System.err.println("今日已打卡");
+        } else {
+            System.err.println("打卡成功");
+        }
+        return result;
+    }
+
+
     public static boolean register(String name, String pass) {
         String sql = "select username from user where username='" + name + "'";
         if (executeQuery(sql)) {
             System.err.println("用户名已经存在,name=" + name);
             return false;
         }
-        sql = "insert into user(username,password) VALUE('" + name + "','" + pass + "')";
+        sql = "insert into user(username,password,qx) VALUE('" + name + "','" + pass + "','0')";
         System.err.println(sql);
         boolean result = executeUpdate(sql);
         if (result) {
@@ -79,7 +94,7 @@ public class BaseDao {
     public static boolean insert(String name,String time,String num,String content)
     {
         String sql="insert into daka(username,dakatime,num,remark) VALUE('" + name + "','" + time + "','" + num + "','" + content + "')";
-        System.err.println(sql);
+    //    System.err.println(sql);
         boolean result = executeUpdate(sql);
         if (result) {
             System.err.println("打卡成功");
@@ -88,6 +103,8 @@ public class BaseDao {
         }
         return result;
     }
+
+
 
     public static boolean executeUpdate(String sql) {
         try {
--- web/jsp/daka.jsp
+++ web/jsp/daka.jsp
@@ -20,12 +20,20 @@
     String content = request.getParameter("content");
     String username =(String)session.getAttribute("name");
     SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");//设置日期格式
-    if (BaseDao.insert(username,df.format(new Date()),"1",content)){
-        out.println("打卡成功！");
+    SimpleDateFormat df1 = new SimpleDateFormat("yyyy-MM-dd 00:00:00");
+    SimpleDateFormat df2 = new SimpleDateFormat("yyyy-MM-dd 23:59:59");
+    if(BaseDao.isdaka(username,df1.format(new Date()),df2.format(new Date()))) {
+        out.println("今日已打卡，请明日再来！");
         out.println("<a href='../index.jsp'>返回首页</a>");
-    } else {
-        out.println("打卡失败，请重新打卡！");
-        out.println("<a href='/html/login.html'>重新登陆</a>");
+    }
+    else {
+        if (BaseDao.insert(username, df.format(new Date()), "1", content)) {
+            out.println("打卡成功！");
+            out.println("<a href='../index.jsp'>返回首页</a>");
+        } else {
+            out.println("打卡失败，请重新打卡！");
+            out.println("<a href='/html/login.html'>重新登陆</a>");
+        }
     }
 %>
 </div>
